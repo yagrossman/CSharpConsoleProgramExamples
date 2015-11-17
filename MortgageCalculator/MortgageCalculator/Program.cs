94254cdbd97f7afda48499fee7697d0a196a090c
Yehuda Grossman <yagrossman@outlook.com>
2015-11-17 13:16:15 -0500

    read in house price from user and convert to int

---
+++ MortgageCalculator/MortgageCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageCalculator
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         //Get house price from user
+         Console.WriteLine("Please enter the house price:");
+         string inputHousePrice = Console.ReadLine();
+         int housePrice = int.Parse(inputHousePrice);
+      }
+   }
+}
