25c1f2f27a112c8ab768556934200578394f11b4
Yehuda Grossman <yagrossman@outlook.com>
2015-11-18 17:20:06 -0500

    added output to make clearer to user, try again

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -12,9 +12,11 @@ namespace GuessingGame
       {
          int number = new Random().Next(1, 10); //Generate number from 1 to 9
          Console.WriteLine("Please geuss the number between 1 and 10");
-         int userGuess = int.Parse(Console.ReadLine());
-         for (int trys = 1; trys <= 5; trys++)
+         //int userGuess = int.Parse(Console.ReadLine());
+         for (int @try = 1; @try <= 5; @try++)
          {
+            if(@try>1)Console.WriteLine("Try Again, enter a number between 1 and 10");
+            int userGuess = int.Parse(Console.ReadLine());
             if (userGuess == number)
             {
                Console.WriteLine("You Won! Way to Go!");
@@ -22,7 +24,7 @@ namespace GuessingGame
             }
             else
             {
-               Console.WriteLine("Sorry, Press Enter to try Again. You have:" + (5 - trys) + "left.");
+               Console.WriteLine("Sorry, Press Enter to try Again. You have:" + (5 - @try) + "left.");
                Console.ReadLine();
             }
          }
