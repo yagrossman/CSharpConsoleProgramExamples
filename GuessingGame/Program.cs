d018f942e4f18a706f8178073affed7806db62e0
Yehuda Grossman <yagrossman@outlook.com>
2015-11-18 17:17:17 -0500

    added for loop for trys

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -13,6 +13,19 @@ namespace GuessingGame
          int number = new Random().Next(1, 10); //Generate number from 1 to 9
          Console.WriteLine("Please geuss the number between 1 and 10");
          int userGuess = int.Parse(Console.ReadLine());
+         for (int trys = 1; trys <= 5; trys++)
+         {
+            if (userGuess == number)
+            {
+               Console.WriteLine("You Won! Way to Go!");
+               break;//no need to try again
+            }
+            else
+            {
+               Console.WriteLine("Sorry, Press Enter to try Again. You have:" + (5 - trys) + "left.");
+               Console.ReadLine();
+            }
+         }
 
       }
    }
