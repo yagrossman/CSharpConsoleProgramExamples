2a9f70cc27587ebc14ad32b7a5523ceb07f7d1aa
Yehuda Grossman <yagrossman@outlook.com>
2015-11-18 17:29:07 -0500

    new logic

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -11,21 +11,20 @@ namespace GuessingGame
       static void Main(string[] args)
       {
          int number = new Random().Next(1, 10); //Generate number from 1 to 9
-         Console.WriteLine("Please geuss the number between 1 and 10");
+         Console.WriteLine("Ready to Play??? :) Guess the number between 1 and 10");
          //int userGuess = int.Parse(Console.ReadLine());
-         for (int @try = 1; @try <= 5; @try++)
+         for (int attempt = 1; attempt <= 5; attempt++)
          {
-            if(@try>1)Console.WriteLine("Try Again, enter a number between 1 and 10");
+            if (attempt > 1)
+            {
+               Console.WriteLine("Sorry, You have: " + (5 - attempt) + " attempts left.");
+               Console.WriteLine("Enter a number between 1 and 10 to try again.")
+            }
             int userGuess = int.Parse(Console.ReadLine());
             if (userGuess == number)
             {
                Console.WriteLine("You Won! Way to Go!");
                break;//no need to try again
-            }
-            else
-            {
-               Console.WriteLine("Sorry, Press Enter to try Again. You have: " + (5 - @try) + "tries left.");
-               Console.ReadLine();
             }
          }
 
