a9af79acc89be542ed31d8f05a6d0a37698fa51d
Yehuda Grossman <yagrossman@outlook.com>
2015-11-18 17:13:20 -0500

    genreate number to be guessed. ask and read in users guess

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -10,6 +10,10 @@ namespace GuessingGame
    {
       static void Main(string[] args)
       {
+         int number = new Random().Next(1, 10); //Generate number from 1 to 9
+         Console.WriteLine("Please geuss the number between 1 and 10");
+         int userGuess = int.Parse(Console.ReadLine());
+
       }
    }
 }
