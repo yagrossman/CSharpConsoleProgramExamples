b840f7e870ab77ea9ff1d212f1fa0199623aac9e
Yehuda Grossman <yagrossman@outlook.com>
2015-11-19 12:22:38 -0500

    read catalog file into array

--- MortgageCalculator/ShoppingAppExample/Program.cs
+++ MortgageCalculator/ShoppingAppExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,21 @@ namespace ShoppingAppExample
       static void Main(string[] args)
       {
       }
+
+      //reads catalog file into an array
+      static string[] ReadCatalogFromFile()
+      {
+         StreamReader input = new StreamReader("catalog.txt");
+         string[] catalog = new string[200];
+         string line = "";
+         for (int i = 0; i < 200; i++)
+         {
+            line = input.ReadLine();
+            if (line == null) break;
+         }
+         return catalog;
+      }
+
+
    }
 }
