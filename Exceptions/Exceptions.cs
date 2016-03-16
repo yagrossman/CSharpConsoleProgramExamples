97c821b93136b0dcaf60dea2dc4717cc733c9370
Yehuda Grossman <yagrossman@outlook.com>
2016-03-16 14:37:37 -0400

    added params to refANDout and changed its name to clarify that

---
+++ Exceptions/Exceptions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+   class Exceptions
+   {
+      static void Main(string[] args)
+      {
+         //try {
+            StreamReader myReader = new StreamReader("\\b00\\Values.txt");
+            string line = "";
+
+            while (line != null)
+            {
+               line = myReader.ReadLine();
+               if (line != null)
+                  Console.WriteLine(line);
+            }
+            myReader.Close(); //needed to remove the reference to the file
+         //}
+
+         //catch (DirectoryNotFoundException e)
+         //{
+         //   Console.WriteLine("Couldn't find the file. Are you sure the DIRECTORY exists?");
+         //}
+         //catch(FileNotFoundException e)
+         //{
+         //   Console.WriteLine("Couldn't find the file. Are you sure you're looking for the correct file?");
+         //}
+         //catch (Exception e)
+         //{
+         //   Console.WriteLine("Something didnt quite work: {0}", e.Message);
+         //}
+         //finally
+         //{
+         //   //Perform any cleanup to roll back the data or close connections
+         //   //to files, database, network, etc.
+         //}
+
+         Console.ReadLine();
+      }
+   }
+}
---
+++ refANDout/refOutParams.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refANDout
+{
+   class refOutParams
+   {
+      static void Main(string[] args)
+      {
+         int number = 20;
+         AddFive(number);
+         Console.WriteLine(number);
+         Console.ReadKey();
+         //GreetPersons(0);
+         //GreetPersons(25, "John", "Jane", "Tarzan");
+         //Console.ReadKey();
+      }
+
+      static void AddFive(int number)
+      {
+         number = number + 5;
+      }
+
+      static void GreetPersons(int someUnusedParameter, params string[] names)
+      {
+         foreach (string name in names)
+            Console.WriteLine("Hello, " + name);
+      }
+   }
+}
