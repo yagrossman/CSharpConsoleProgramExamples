eb066e7b4090c8469635fe2314df58272eefb4ad
Yehuda Grossman <yagrossman@outlook.com>
2016-03-16 13:45:45 -0400

    exceptions teaching example

--- Strings/StringsDateTime.cs
+++ Strings/StringsDateTime.cs
@@ -11,18 +11,28 @@ namespace Strings
       static void Main(string[] args)
       {//String Stuff
          string myString = "Go to your c:\\ drive";
+         Console.WriteLine(myString);
          myString = "My \"so called\" life";
+         //https://blogs.msdn.microsoft.com/csharpfaq/2004/03/12/what-character-escape-sequences-are-available/
          myString = "What if i need \n a new line?";
+         Console.WriteLine(myString);
          myString = string.Format("Make: {0} (Model: {1})", "BMW", "760li");
+         Console.WriteLine(myString);
          myString = string.Format("{0:C}", 123.45);//Dollars and Cents
+         Console.WriteLine(myString);
          myString = string.Format("{0:N}", 13456789);//Number with commas and decimal point 
+         Console.WriteLine(myString);
          myString = string.Format("{0:P}", .123);//Percentage
+         Console.WriteLine(myString);
          myString = string.Format("Phone Number: {0:(###)-###-####}", 7737912243);//Custom Format for phone numbers
-
+         Console.WriteLine(myString);
          myString = " That summer we took threes accross the board  ";
          //myString = myString.Substring(5, 14);
-         //myString = myString.ToUpper();
+         Console.WriteLine(myString);
+         myString = myString.ToUpper();
+         Console.WriteLine(myString);
          //myString = myString.Replace(" ", "--");
+         Console.WriteLine(myString);
          myString = String.Format("Length before: {0} -- After: {1}", myString.Length, myString.Trim().Length);
          Console.WriteLine(myString);
 
--- SunTime_Class/Classes.cs
+++ SunTime_Class/Classes.cs
@@ -42,6 +42,7 @@ namespace Classes2
    abstract class Vehicle
    {
       public string Make { get; set; }
+      //public string VIN { get; set; }
       public string Model { get; set; }
       public int Year { get; set; }
       public string Color { get; set; }
