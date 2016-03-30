9f79d2569046f867dca8e356fe81020819b8d988
Yehuda Grossman <yagrossman@outlook.com>
2016-03-30 15:35:06 -0400

    add more examples to show in lecture

--- LINQ/LINQ.cs
+++ LINQ/LINQ.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LINQ
 {
-   class Program
+   class LINQ
    {
       static void Main(string[] args)
       {
@@ -18,7 +18,31 @@ namespace LINQ
                 new Car() {Make="BMW", Model="55i", Color=CarColor.Black, StickerPrice=57000, Year=2010}
             };
 
-         foreach (var car in myCars)
+         //LINQ query to filter only bmws && 2010 models from our Car list myCars
+         /*
+         var bmws = from car in myCars
+                    where car.Make == "BMW"
+                    && car.Year == 2010
+                    select car;
+         */
+         /*
+         var bmws = from car in myCars        //this creates new anonymous collection with these 3 types
+                      where car.Make == "BMW"
+                      select new { car.Make, car.Model, car.Year };*/
+
+         /* //here we can order the cars by their year
+         var orderedCars = from car in myCars
+                           orderby car.Year descending //default is ascending
+                           select car;
+                           */
+
+         //same examples using the "method" syntax instead of the LINQ query syntax (same exact output)
+         //var _bmws = myCars.Where(p => p.Year == 2010).Where(p => p.Make == "BMW"); //pass in lambda expressions
+         //var _orderedCars = myCars.OrderByDescending(p => p.Year);
+         //var sum = myCars.Sum(p => p.StickerPrice);
+         //Console.WriteLine(sum);
+
+         foreach (var car in myCars) //var is implicit and lets the compiler figure out which type to use
             Console.WriteLine("{0} - {1} - {2}", car.Make, car.Model, car.Year);
 
          Console.ReadLine();
