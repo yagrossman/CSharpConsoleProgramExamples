1e0c03bcfb2c07c7bcb76f777e0ea342e29d692a
Yehuda Grossman <yagrossman@outlook.com>
2016-04-05 14:24:28 -0400

    Add GameWinCheck method to Game class Add partial main method logic flow

--- BlackjackGame/Game.cs
+++ BlackjackGame/Game.cs
@@ -11,7 +11,7 @@ namespace BlackjackGame
       bool UserWon;
       bool ComputerWon;
       int UserScore;
-      int ComputerScore;
+      public int ComputerScore;
       Deck Deck;
 
       public Game()  // Game Constructor
@@ -39,6 +39,21 @@ namespace BlackjackGame
          Console.WriteLine("User drew a {0} and now has {1} points.", userCard.GetFace(), UserScore);
          if (UserScore == 21) UserWon = true;
          if (UserScore > 21) ComputerWon = true;
+      }
+
+      public bool GameWinCheck()
+      {
+         if (ComputerWon)
+         {
+            Console.WriteLine("Game over, Computer Won! The house always wins in the end! ;)");
+            return true;
+         }
+         if (UserWon)
+         {
+            Console.WriteLine("Game over, You Won! Great job sticking it to The Man! :)");
+            return true;
+         }
+         return false;
       }
 
    }
--- BlackjackGame/Program.cs
+++ BlackjackGame/Program.cs
@@ -10,15 +10,40 @@ namespace BlackjackGame
    {
       static void Main(string[] args)
       {
-         Deck myDeck = new Deck();
-         Card card1 = myDeck.DrawCard();
-         Console.WriteLine(card1.GetFace());
-         Console.WriteLine(myDeck.DrawCard().GetFace());
-         Console.WriteLine("for loop");
-         for (int i = 0; i < 52; i++)
+         Console.WriteLine("Welcome to Blackjack! Press any key to begin game.");
+         Console.ReadKey();
+         Game myGame = new Game();
+         myGame.ComputerMove();
+         myGame.ComputerMove();
+         while (myGame.ComputerScore < 17)
          {
-            Console.WriteLine(myDeck.DrawCard().GetFace());
+            myGame.ComputerMove();
          }
+         if (myGame.GameWinCheck())
+         {
+            Console.WriteLine("Press any key to exit.");
+            Environment.Exit(1);
+         }
+         myGame.UserMove();
+         myGame.UserMove();
+         while (myGame.GameWinCheck() == false)
+         {
+            Console.WriteLine("Would you like to draw another card (Y/N)?");
+            if (Console.ReadLine().ToUpper() == "Y")
+            {
+               myGame.UserMove();
+               if (myGame.GameWinCheck())
+               {
+                  Console.WriteLine("Press any key to exit.");
+                  Environment.Exit(1);
+               }
+            }
+
+               
+         }
+         myGame.ComputerMove();
+         myGame.UserMove();
+
          Console.ReadLine();
 
       }
